de5874f964272d597a6be76c4c1fb609602a46dd
Prince <banerjee.prince69@gmail.com>
2024-08-27 01:38:44 +0530

    Getting IP from domain

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Main {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
 
         InetAddress IP = InetAddress.getByName("www.ocsaly.com");
-        System.out.println(IP);
+
+        System.out.println("HOST ADDRESS        :" +IP.getHostAddress());
+        System.out.println("CANONICAL HOST NAME : "+IP.getCanonicalHostName());
+        System.out.println("HOST NAME           : "+IP.getHostName());
 
     }
 }
