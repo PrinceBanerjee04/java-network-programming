e131bb626f33646f2f45c18ae7b8e105fbf9029f
Prince <banerjee.prince69@gmail.com>
2024-08-31 11:29:00 +0530

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -33,24 +33,24 @@ public class Main {
 //            System.out.println(exception.getMessage());
 //        }
 
-        try{
-            URL url = new URL("https://ocsaly.com");
-            URLConnection urlConnection = url.openConnection();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            ReadableByteChannel readableByteChannel = Channels.newChannel(inputStream);
-            ByteBuffer buffer = ByteBuffer.allocate(64);
-
-            String line =  null;
-            while(readableByteChannel.read(buffer) > 0){
-                System.out.println(new String(buffer.array()));
-                buffer.clear();
-            }
-
-            readableByteChannel.close();
-
-        } catch (IOException ioException){
-            System.out.println(ioException.getMessage());
-        }
+//        try{
+//            URL url = new URL("https://ocsaly.com");
+//            URLConnection urlConnection = url.openConnection();
+//
+//            InputStream inputStream = urlConnection.getInputStream();
+//            ReadableByteChannel readableByteChannel = Channels.newChannel(inputStream);
+//            ByteBuffer buffer = ByteBuffer.allocate(64);
+//
+//            String line =  null;
+//            while(readableByteChannel.read(buffer) > 0){
+//                System.out.println(new String(buffer.array()));
+//                buffer.clear();
+//            }
+//
+//            readableByteChannel.close();
+//
+//        } catch (IOException ioException){
+//            System.out.println(ioException.getMessage());
+//        }
     }
 }
